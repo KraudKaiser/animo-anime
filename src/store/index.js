7f5debcc357a6650bcd36d7a9a3fc8ce488a8ccc
ParadigmaRama <ramirogimenezcontacto@gmail.com>
2023-03-08 11:36:39 -0300

    comentarios en store

--- src/store/index.js
+++ src/store/index.js
@@ -4,6 +4,8 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
+	//no se puede mutar (cambiar) state facilmentet.
+	// para ello hay que usar mutations
   state: {
 	links:[
 		{title:"Inicio", path:"/"},
@@ -13,11 +15,19 @@ export default new Vuex.Store({
 	],
 	routes:["/","/directory", "/features","/about"]
   },
-  getters: {
-  },
+  //se utiliza para cambiar las variables en state
+  //reciben siempre como primer parametro state
+  //actua sincronamente (no .then o async)
   mutations: {
   },
+  //Actions es para cambiar las variables de state async
+  //pero debe hacer commit de las variables en mutations
+  //reciben contexto, destructurandolo a commit
   actions: {
+  },
+
+  //funcionan como las computed properties.
+  getters: {
   },
   modules: {
   }
